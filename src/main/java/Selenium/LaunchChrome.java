eaf3facce5f6a5aa754764a4be24a0f6f39cf3d8
Shraddha Pawar <shraddhapawar1988@gmail.com>
2022-06-08 11:23:30 +0200

    first selenium project

---
+++ src/main/java/Selenium/LaunchChrome.java
@@ -0,0 +1,18 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:/Users/HP/Downloads/chromedriver_win32/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+		driver.close();
+
+	}
+
+}
